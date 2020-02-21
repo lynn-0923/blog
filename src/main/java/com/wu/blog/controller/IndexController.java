9b0244bd3a839039e704418de8b71333b46cbbfc
wuliang0515 <1528141204.com>
2020-02-21 13:26:50 +0800

    add flyway migration

--- src/main/java/com/wu/blog/controller/IndexController.java
+++ src/main/java/com/wu/blog/controller/IndexController.java
@@ -1,14 +1,32 @@
 package com.wu.blog.controller;
 
+import com.wu.blog.domain.User;
+import com.wu.blog.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public  String index(){
+    public  String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies
+             ) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+              User user = userMapper.findUserByToken(token);
+              if (user !=null){
+                  request.getSession().setAttribute("user",user);
+              }
+                break;
+            }
+        }
         return "index";
     }
 }
