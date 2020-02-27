d76e0245fb047fb59d008d8320492767a9a7f68d
wuliang0515 <1528141204.com>
2020-02-27 19:40:42 +0800

    change sql

--- src/main/resources/db/migration/V1__create_user_table.sql
+++
@@ -1,9 +0,0 @@
-  create table USER
-        (
-          ID bigint auto_increment  primary key,
-          ACCOUNT_ID   VARCHAR(100),
-          NAME         VARCHAR(50),
-          TOKEN        CHAR(36),
-          GMT_CREATE   BIGINT,
-          GMT_MODIFIED BIGINT
-        );
